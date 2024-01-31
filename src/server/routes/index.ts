a3fbdcc25ba5d6f7f3085e79fb02716401f978cb
Luan <luan.evieira43@gmail.com>
2024-01-31 13:01:34 -0300

    Add Controllers

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { CidadesController } from '../controllers';
 
 const router = Router();
 
@@ -7,8 +8,6 @@ router.get('/', (req, res) => {
   return res.send('Olá, DEV!');
 });
 
-router.post('/teste', (req, res) => {
-  return res.status(StatusCodes.OK).json(req.body);
-});
+router.post('/cidades', CidadesController.create);
 
 export { router };

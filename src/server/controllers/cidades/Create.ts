441035abe96f95004c34dbece17520226a8021d1
Luan <luan.evieira43@gmail.com>
2024-02-01 12:49:39 -0300

    Add Schema Yup

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,13 +1,31 @@
 import { Request, Response } from "express";
+import * as yup from 'yup';
 
 interface ICidade {
   nome: string;
 }
 
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+});
 
-  const data = req.body;
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+  let validatedData: ICidade | undefined = undefined;
+
+  try {
+    validatedData = await bodyValidation.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+
+    return res.json({
+      errors: {
+        default: yupError.message,
+      }
+    });
+  }
+
+  const data = req.body.nome;
   console.log("~ data:", data)
 
-  return res.send(`Create ${req.body}!`);
+  return res.send(`Create ${validatedData}!`);
 };
